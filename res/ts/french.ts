3d9eed263997f7f2948b14396594e73184e0ee0e
a-lemonnier <40183590+a-lemonnier@users.noreply.github.com>
2020-09-01 12:40:07 +0200

    First commit

---
+++ res/ts/french.ts
@@ -0,0 +1,205 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR" sourcelanguage="en">
+<context>
+    <name>MainWIndow</name>
+    <message>
+        <location filename="../../mainwindow.ui" line="75"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="395"/>
+        <source>Path:</source>
+        <translation>Chemin:</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="90"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="397"/>
+        <source>Path to the directory.</source>
+        <translation type="unfinished">Chemin vers le repertoire.</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="93"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="399"/>
+        <source>./</source>
+        <translation type="unfinished">./</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="119"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="401"/>
+        <source>Select a directory.</source>
+        <translation type="unfinished">Choisir un repertoire.</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="122"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="403"/>
+        <source>Browse</source>
+        <translation type="unfinished">Parcourir</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="148"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="404"/>
+        <source>Year:</source>
+        <translation type="unfinished">Année:</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="155"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="406"/>
+        <source>Set number of years.</source>
+        <translation type="unfinished">Définir le nombre d&apos;années.</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="181"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="408"/>
+        <source>Day:</source>
+        <translation type="unfinished">Jour:</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="188"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="410"/>
+        <source>Set number of days.</source>
+        <translation type="unfinished">Définir le nombre de jours.</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="214"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="412"/>
+        <source>Hour: </source>
+        <translation type="unfinished">Heure: </translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="221"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="414"/>
+        <source>Set number of hours.</source>
+        <translation type="unfinished">Définir le nombre d&apos;heures.</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="247"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="416"/>
+        <source>Min: </source>
+        <translation type="unfinished">Min: </translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="254"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="418"/>
+        <source>Set number of minutes.</source>
+        <translation type="unfinished">Définir le nombre de minutes.</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="280"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="420"/>
+        <source>Sec: </source>
+        <translation type="unfinished">Sec: </translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="287"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="422"/>
+        <source>Set number of seconds.</source>
+        <translation type="unfinished">Définir le nombre de secondes.</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="325"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="425"/>
+        <source>Enable DST.</source>
+        <translation type="unfinished">Activer l&apos;heure d&apos;été.</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="328"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="427"/>
+        <source>DST</source>
+        <translation type="unfinished">Heure d&apos;été</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="367"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="429"/>
+        <source>Change all timsestamps.</source>
+        <translation type="unfinished">Modifier les horaires.</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="370"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="431"/>
+        <source>Run</source>
+        <translation type="unfinished">Lancer</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="390"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="433"/>
+        <source>Discard changes.</source>
+        <translation type="unfinished">Quitter dans changements.</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="393"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="435"/>
+        <source>Quit</source>
+        <translation type="unfinished">Quitter</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="413"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="437"/>
+        <source>Reset fields.</source>
+        <translation type="unfinished">Vider les champs.</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="416"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="439"/>
+        <source>Reset</source>
+        <translation type="unfinished">Reset</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="436"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="440"/>
+        <source>Test</source>
+        <translation type="unfinished">Test</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="456"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="441"/>
+        <source>About</source>
+        <translation>A propos</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="481"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="443"/>
+        <source>Progression.</source>
+        <translation type="unfinished">Progression.</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="493"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="445"/>
+        <source>Auto wrap lines</source>
+        <translation type="unfinished">Retour à la ligne automatique</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="500"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="446"/>
+        <source>Auto scroll</source>
+        <translation type="unfinished">Défilement auto</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="507"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="447"/>
+        <source>Quiet</source>
+        <translation type="unfinished">Silence</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="533"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="449"/>
+        <source>Zoom in</source>
+        <translation type="unfinished">Zoomer</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="539"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="454"/>
+        <source>+</source>
+        <translation type="unfinished">+</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="552"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="456"/>
+        <source>Zoom out</source>
+        <translation type="unfinished">Dézoomer</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="555"/>
+        <location filename="../../build/shiftpicdate-gui_autogen/include/ui_mainwindow.h" line="458"/>
+        <source>-</source>
+        <translation>-</translation>
+    </message>
+</context>
+</TS>
