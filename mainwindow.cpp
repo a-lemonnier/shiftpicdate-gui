285690675ee1e562c3ffd4a0fc1716509428d19b
a-lemonnier <audric.lemonnier@obspm.fr>
2020-09-10 10:07:46 +0200

    Update

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -222,7 +222,7 @@ void MainWIndow::on_bRot_clicked() {
         QMatrix rm;
         rm.rotate(this->iPicRot);
 
-        QPixmap cPmap=ui->picLabel->pixmap(Qt::ReturnByValueConstant::ReturnByValue); 
+        QPixmap cPmap=ui->picLabel->pixmap(Qt::ReturnByValueConstant(0)); //Qt::ReturnByValue
         cPmap = cPmap.transformed(rm);
 
         ui->picLabel->setPixmap(cPmap);
